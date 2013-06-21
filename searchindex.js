243efa2f243cd397c835060156f8017075537077
José Luis Lafuente <jlafuente@ubimet.com>
2013-06-21 16:15:15 +0200

    Autogenerated github-pages

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objtypes:{},titles:["doc2git &#8211; Generate content and push it to git"],filenames:["index"],envversion:43,objects:{},terms:{tool:0,helper:0,remot:0,repositori:0,"static":0},titleterms:{gener:0,doc2git:0,push:0,git:0,content:0},objnames:{}})
+Search.setIndex({objtypes:{},objnames:{},titles:["doc2git &#8211; Generate content and push it to git"],filenames:["index"],titleterms:{content:0,gener:0,git:0,push:0,doc2git:0},terms:{helper:0,tool:0,remot:0,repositori:0,"static":0},envversion:43,objects:{}})
